314a78140ab214da887b2ef2f1f27e42c45682dc
Loïc Boucher <loic.boucher@iut-dijon.u-bourgogne.fr>
2023-09-14 11:25:56 +0200

    Ajout des classes issues de la conception

---
+++ Server/Data/IUserDao.cs
@@ -0,0 +1,6 @@
+﻿namespace AutomateDesign.Server.Data
+{
+    public interface IUserDao : BaseDao
+    {
+    }
+}
---
+++ Server/Model/EmailVerification.cs
@@ -0,0 +1,10 @@
+﻿namespace AutomateDesign.Server.Model
+{
+    public class EmailVerification
+    {
+        private int verificationCode;
+        private string subject;
+        private string recipient;
+        private string message;
+    }
+}
