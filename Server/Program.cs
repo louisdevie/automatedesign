5159aebc11483bf17fa484d190106f5a54c9ad88
Louis Devie <Louis_Devie@etu.u-bourgogne.fr>
2023-10-10 08:47:37 +0200

    Ajout du gRPC

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,17 +1,26 @@
+using AutomateDesign.Server.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddGrpc();
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5001, options => {
+        options.Protocols = HttpProtocols.Http2;
+        options.UseHttps();
+    });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 
-app.UseHttpsRedirection();
+app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapControllers();
+app.MapGrpcService<GreeterService>();
 
 app.Run();
