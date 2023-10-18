b18dc1d739dfb6fbd2ca46ba196a02e00be1e5fe
Loïc Boucher <loic.boucher@iut-dijon.u-bourgogne.fr>
2023-10-18 10:20:36 +0200

    Initial Commit Branche

---
+++ Server/Data/IAutomateDao.cs
@@ -0,0 +1,17 @@
+﻿namespace AutomateDesign.Server.Data
+{
+    public interface IAutomateDao
+    {
+        public Session Create(Session session);
+
+        public Session ReadByToken(string token);
+
+        /// <summary>
+        /// Mets à jour la date de dernière utilisation.
+        /// </summary>
+        /// <param name="session">La session à mettre à jour.</param>
+        public void UpdateLastUse(Session session);
+
+        public void Delete(string token);
+    }
+}
