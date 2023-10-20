f52994fa20fb0c52ffb6af2514826fab1fbd9de1
Loïc Boucher <loic.boucher@iut-dijon.u-bourgogne.fr>
2023-10-20 10:34:56 +0200

    Suppresion comit précédent (problème de communication au seins du groupe)

--- Client/View/EditAutomateView.xaml.cs
+++ Client/View/EditAutomateView.xaml.cs
@@ -67,71 +67,7 @@ namespace AutomateDesign.Client.View
 
         private void AddStateButtonClick(object sender, RoutedEventArgs e)
         {
-            // Créez un Grid pour organiser le cercle et le texte
-            Grid grid = new Grid();
-            grid.Width = 100; 
-            grid.Height = 100;
-            grid.HorizontalAlignment = HorizontalAlignment.Center;
-            grid.VerticalAlignment = VerticalAlignment.Center;
-
-            // Créez un Ellipse (cercle)
-            Ellipse cercle = new Ellipse
-            {
-                Width = 100,
-                Height = 100,
-                Stroke = Brushes.Black, // Couleur du bord (noir)
-                StrokeThickness = 2, // Épaisseur du trait
-                Fill = Brushes.White, // Couleur de remplissage (blanc)
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-            };
-
-            // Créez un TextBlock pour ajouter du texte au cercle
-            TextBox texte = new TextBox
-            {
-                Text = "Etat Test",
-                Foreground = Brushes.Black, // Couleur du texte (noir)
-                HorizontalAlignment = HorizontalAlignment.Center,
-                VerticalAlignment = VerticalAlignment.Center,
-            };
-
-            // Gestionnaire d'événement lorsque la souris est enfoncée sur le Grid
-            grid.MouseLeftButtonDown += (s, e) =>
-            {
-                lastMousePosition = e.GetPosition(grid);
-                grid.CaptureMouse();
-            };
-
-            // Gestionnaire d'événement lorsque la souris est déplacée
-            grid.MouseMove += (s, e) =>
-            {
-                if (grid.IsMouseCaptured)
-                {
-                    Point currentPosition = e.GetPosition(grid);
-                    double offsetX = currentPosition.X - lastMousePosition.X;
-                    double offsetY = currentPosition.Y - lastMousePosition.Y;
-
-                    // Déplacez le Grid en fonction de la différence de position
-                    Canvas.SetLeft(grid, Canvas.GetLeft(grid) + offsetX);
-                    Canvas.SetTop(grid, Canvas.GetTop(grid) + offsetY);
-
-                    lastMousePosition = currentPosition;
-                }
-            };
-
-            // Gestionnaire d'événement lorsque le bouton de la souris est relâché
-            grid.MouseLeftButtonUp += (s, e) =>
-            {
-                grid.ReleaseMouseCapture();
-            };
-
-            // Ajoutez le cercle et le texte au Grid
-            grid.Children.Add(cercle);
-            grid.Children.Add(texte);
-
-            // Ajoutez le Grid au Canvas
-            ZoneDessinAutomate.Children.Add(grid);
-
+            
         }
 
         
