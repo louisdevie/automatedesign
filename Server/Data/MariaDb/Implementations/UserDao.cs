47ada9c04658f55410bc757f92dfcd5030aa4d29
Loïc Boucher <loic.boucher@iut-dijon.u-bourgogne.fr>
2023-11-13 14:37:30 +0100

    Documentation des interfaces du DAO IAutomateDao ISessionDao IUserDao

--- Server/Data/MariaDb/Implementations/UserDao.cs
+++ Server/Data/MariaDb/Implementations/UserDao.cs
@@ -9,11 +9,6 @@ namespace AutomateDesign.Server.Data.MariaDb.Implementations
     {
         public UserDao(DatabaseConnector connector) : base(connector) { }
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="user"></param>
-        /// <returns></returns>
         public User Create(User user)
         {
             using MySqlConnection connection = Connect();
