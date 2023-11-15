2f267865236de7fd4e5f2637ae801951ea431002
Loïc Boucher <loic.boucher@iut-dijon.u-bourgogne.fr>
2023-11-15 08:39:24 +0100

    Tests sur Core.User

---
+++ TestsUnitaires/Core/RegistrationTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Net.Mail;
+using AutomateDesign.Core.Users;
+using Xunit;
+
+namespace TestsUnitaires.Core
+{
+    public class RegistrationTest
+    {
+        [Fact]
+        public void Constructor_WithVerificationCodeAndExpirationAndUser_PropertiesSetCorrectly()
+        {
+            // Arrange
+            uint verificationCode = 1234;
+            DateTime expiration = DateTime.UtcNow.AddHours(1);
+            User user = new User(new MailAddress("test@example.com"), HashedPassword.FromPlain("hashedPassword"));
+
+            // Act
+            Registration registration = new Registration(verificationCode, expiration, user);
+
+            // Assert
+            Assert.Equal(verificationCode, registration.VerificationCode);
+            Assert.Equal(expiration, registration.Expiration);
+            Assert.Equal(user, registration.User);
+        }
+
+        [Fact]
+        public void Constructor_WithUser_GeneratesVerificationCodeAndSetsExpirationAndUser()
+        {
+            // Arrange
+            User user = new User(new MailAddress("test@example.com"), HashedPassword.FromPlain("hashedPassword"));
+
+            // Act
+            Registration registration = new Registration(user);
+
+            // Assert
+            Assert.True(registration.VerificationCode >= 1000 && registration.VerificationCode <= 9999);
+            Assert.True(registration.Expiration > DateTime.UtcNow);
+            Assert.Equal(user, registration.User);
+        }
+
+        [Fact]
+        public void Expired_BeforeExpiration_ReturnsFalse()
+        {
+            // Arrange
+            User user = new User(new MailAddress("test@example.com"), HashedPassword.FromPlain("hashedPassword"));
+            Registration registration = new Registration(user);
+
+            // Act & Assert
+            Assert.False(registration.Expired);
+        }
+
+        [Fact]
+        public void Expired_AfterExpiration_ReturnsTrue()
+        {
+            // Arrange
+            User user = new User(new MailAddress("test@example.com"), HashedPassword.FromPlain("hashedPassword"));
+            Registration registration = new Registration(1234, DateTime.UtcNow.AddSeconds(-1), user);
+
+            // Act & Assert
+            Assert.True(registration.Expired);
+        }
+
+        [Fact]
+        public void WithUser_ReturnsNewRegistrationWithUpdatedUser()
+        {
+            // Arrange
+            uint verificationCode = 1234;
+            DateTime expiration = DateTime.UtcNow.AddHours(1);
+            User user = new User(new MailAddress("test@example.com"), HashedPassword.FromPlain("hashedPassword"));
+            Registration originalRegistration = new Registration(verificationCode, expiration, user);
+
+            // Act
+            User newUser = new User(new MailAddress("newtest@example.com"), HashedPassword.FromPlain("newhashedPassword"));
+            Registration newRegistration = originalRegistration.WithUser(newUser);
+
+            // Assert
+            Assert.Equal(verificationCode, newRegistration.VerificationCode);
+            Assert.Equal(expiration, newRegistration.Expiration);
+            Assert.Equal(newUser, newRegistration.User);
+        }
+    }
+}
---
+++ TestsUnitaires/Core/UserTest.cs
@@ -0,0 +1,64 @@
+﻿using AutomateDesign.Core.Exceptions;
+using AutomateDesign.Core.Users;
+using System.Net.Mail;
+using Xunit;
+
+namespace TestsUnitaires.Core
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Constructor_WithValidParameters_PropertiesSetCorrectly()
+        {
+            // Arrange
+            int id = 1;
+            MailAddress email = new MailAddress("test@example.com");
+            HashedPassword password = HashedPassword.FromPlain("hashedPassword");
+            bool isVerified = true;
+
+            // Act
+            User user = new User(id, email, password, isVerified);
+
+            // Assert
+            Assert.Equal(id, user.Id);
+            Assert.Equal(email, user.Email);
+            Assert.Equal(password, user.Password);
+            Assert.Equal(isVerified, user.IsVerified);
+        }
+
+        [Fact]
+        public void Constructor_WithInvalidEmail_ThrowsInvalidResourceException()
+        {
+            // Arrange
+            int id = 1;
+            string invalidEmail = "invalid_email";
+            HashedPassword password = HashedPassword.FromPlain("hashedPassword");
+            bool isVerified = true;
+
+            // Act & Assert
+            Assert.Throws<InvalidResourceException>(() => new User(id, invalidEmail, password, isVerified));
+        }
+
+        [Fact]
+        public void WithId_ReturnsNewUserWithUpdatedId()
+        {
+            // Arrange
+            int originalId = 1;
+            MailAddress email = new MailAddress("test@example.com");
+            HashedPassword password = HashedPassword.FromPlain("hashedPassword");
+            bool isVerified = true;
+
+            User originalUser = new User(originalId, email, password, isVerified);
+
+            // Act
+            int newId = 2;
+            User newUser = originalUser.WithId(newId);
+
+            // Assert
+            Assert.Equal(newId, newUser.Id);
+            Assert.Equal(email, newUser.Email);
+            Assert.Equal(password, newUser.Password);
+            Assert.Equal(isVerified, newUser.IsVerified);
+        }
+    }
+}
