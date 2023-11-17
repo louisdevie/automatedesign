e744bd54b8efd2241e44aaee0d7a069d0e9ed7fb
Louis Devie <louis.devie@iut-dijon.u-bourgogne.fr>
2023-11-17 15:01:51 +0100

    Ajout de l'interface IDocumentSerialiser

---
+++ ClientModel/Serialisation/IDocumentSerialiser.cs
@@ -0,0 +1,21 @@
+﻿using AutomateDesign.Core.Documents;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutomateDesign.Client.Model.Serialisation
+{
+    internal interface IDocumentSerialiser
+    {
+        IAsyncEnumerable<byte[]> SerialiseDocumentAsync(Document document);
+
+        Task<byte[]> SerialiseHeaderAsync(Document document);
+
+        Task<Document> DeserialiseDocumentAsync(IAsyncEnumerable<byte[]> stream);
+
+        IAsyncEnumerable<DocumentHeader> DeserialiseHeadersAsync(IAsyncEnumerable<byte[]> stream);
+    }
+}
