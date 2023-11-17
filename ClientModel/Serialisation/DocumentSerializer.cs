313a66d277aca660b7ce16ef8be3fa2a4d1c92f6
FEVRE Corentin <corentin.fevre@iut-dijon.u-bourgogne.fr>
2023-11-17 15:10:53 +0100

    Classe déplacer sérialisation

---
+++ ClientModel/Serialisation/DocumentSerializer.cs
@@ -0,0 +1,20 @@
+﻿using AutomateDesign.Core.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AutomateDesign.Client.Model.Serialisation
+{
+    public class DocumentSerializer
+    {
+        public byte[] SerializeToUtf8Bytes(Document document)
+        {
+            // Sérialiser la classe en bytes au format UTF-8
+            byte[] jsonUtf8Bytes = JsonSerializer.SerializeToUtf8Bytes(document);
+            return jsonUtf8Bytes;
+        }
+    }
+}
