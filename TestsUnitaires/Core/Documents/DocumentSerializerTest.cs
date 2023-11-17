277d8ea00aafa3f93dd2240b560de2b212bc6fe1
FEVRE Corentin <corentin.fevre@iut-dijon.u-bourgogne.fr>
2023-11-17 15:08:35 +0100

    Sérialisation

---
+++ TestsUnitaires/Core/Documents/DocumentSerializerTest.cs
@@ -0,0 +1,26 @@
+﻿using AutomateDesign.Core.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsUnitaires.Core.Documents
+{
+    public class DocumentSerializerTests
+    {
+        [Fact]
+        public void SerializeToUtf8BytesTest()
+        {
+         
+            var document = new Document();
+            var documentSerializer = new DocumentSerializer();
+
+        
+            byte[] result = documentSerializer.SerializeToUtf8Bytes(document);
+           
+            Assert.NotNull(result);
+            Assert.NotEmpty(result);
+        }
+    }
+}
