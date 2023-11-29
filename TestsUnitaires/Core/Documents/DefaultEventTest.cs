6c5320fb3b92e2856c573bd5bf36eff0dd915805
Loïc Boucher <loic.boucher@iut-dijon.u-bourgogne.fr>
2023-11-29 09:52:24 +0100

    Tests Unitaires DefaultEvent

---
+++ TestsUnitaires/Core/Documents/DefaultEventTest.cs
@@ -0,0 +1,26 @@
+﻿using AutomateDesign.Core.Documents;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestsUnitaires.Core.Documents
+{
+    public class DefaultEventTest
+    {
+        [Fact]
+        public void TestOrder()
+        {
+            DefaultEvent defaultEvent = new DefaultEvent();
+            Assert.Equal(defaultEvent.Order, 1_000_000_000);
+        }
+
+        [Fact]
+        public void TestDisplayName()
+        {
+            DefaultEvent defaultEvent = new DefaultEvent();
+            Assert.Equal(defaultEvent.DisplayName, "*");
+        }
+    }
+}
