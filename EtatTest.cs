e535270cbe27bd78326c0a3dd17ff2a4dd984b83
Loïc Boucher <loic.boucher@iut-dijon.u-bourgogne.fr>
2023-12-12 14:58:21 +0100

    Création des fichiers (classes & structure) qui serveront de base à l'exportation de l'automate en code

---
+++ EtatTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomAutomate.Etats
+{
+    public class EtatX : Etat
+    {
+        public EtatX Transition(Enum e)
+        {
+
+        }
+
+        public void Action(Enum e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
